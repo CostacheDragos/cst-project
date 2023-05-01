6ccb3711ece76968ee8f927e0bf4b7b159ab6f73
Mocanu Alexandra <mocanu.alexandra1504@gmail.com>
2023-05-01 16:11:54 +0300

    User can sort the list of trips

--- Frontend/angular-project/src/app/main/components/trips-dashboard/trips-dashboard.component.ts
+++ Frontend/angular-project/src/app/main/components/trips-dashboard/trips-dashboard.component.ts
@@ -14,7 +14,7 @@ export class TripsDashboardComponent {
   startIndex: number = 0;
   pageLength: number = 6;
   visible = true;
-  
+
   constructor() {}
 
   ngOnInit(): void {
@@ -58,6 +58,95 @@ export class TripsDashboardComponent {
   //Sorting methods
   sortByCityAscending() {
     this.listOfTrips.sort((a, b) => a.city.localeCompare(b.city));
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortByCityDescending() {
+    this.listOfTrips.sort((a, b) => b.city.localeCompare(a.city));
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortByCountryAscending() {
+    this.listOfTrips.sort((a, b) => a.country.localeCompare(b.country));
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortByCountryDescending() {
+    this.listOfTrips.sort((a, b) => b.country.localeCompare(a.country));
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortByDateAscending() {
+    this.listOfTrips.sort((a, b) => {
+      const dateA = a.date.split('/').reverse().join('-'); // rearrange to yyyy-mm-dd format
+      const dateB = b.date.split('/').reverse().join('-'); // rearrange to yyyy-mm-dd format
+      return Date.parse(dateA) - Date.parse(dateB);
+    });
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortByDateDescending() {
+    this.listOfTrips.sort((a, b) => {
+      const dateA = a.date.split('/').reverse().join('-');
+      const dateB = b.date.split('/').reverse().join('-');
+      return Date.parse(dateB) - Date.parse(dateA);
+    });
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortByRatingAscending() {
+    this.listOfTrips.sort((a, b) => a.rating - b.rating);
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortByRatingDescending() {
+    this.listOfTrips.sort((a, b) => b.rating - a.rating);
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortBySpendingAscending() {
+    this.listOfTrips.sort((a, b) => a.spending - b.spending);
+    this.currentPageTrips = this.listOfTrips.slice(
+      this.startIndex,
+      this.pageLength
+    );
+    this.visible = false;
+  }
+
+  sortBySpendingDescending() {
+    this.listOfTrips.sort((a, b) => b.spending - a.spending);
     this.currentPageTrips = this.listOfTrips.slice(
       this.startIndex,
       this.pageLength
