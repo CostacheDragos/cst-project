a0e6cf6958f20ded11be6c61e6c53a3f160a2655
Dinescu Damian <dandreidamian@gmail.com>
2023-05-10 16:58:31 +0300

    Add Users and Trips repositories.

---
+++ Backend/DataLayer/Repositories/TripsRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories
+{
+    public class TripsRepository : RepositoryBase<Trip>
+    {
+        private readonly AppDbContext dbContext;
+
+        public TripsRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
