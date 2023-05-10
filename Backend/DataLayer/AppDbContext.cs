447b4c8e995e08a9326b9ab75b55cd891cf619c3
Dinescu Damian <dandreidamian@gmail.com>
2023-05-10 16:30:55 +0300

    Add initial backend project structure.

---
+++ Backend/DataLayer/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+            .UseSqlServer("Server =.; Database = Travelogs; Trusted_Connection = True; TrustServerCertificate = True")
+                    .LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
---
+++ Backend/DataLayer/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using DataLayer.Repositories;
+
+namespace DataLayer
+{
+    public class UnitOfWork
+    {
+        public UsersRepository Users { get; }
+        public RolesRepository Roles { get; }
+        public TripsRepository Trips { get; }
+
+        private readonly AppDbContext _dbContext;
+
+        public UnitOfWork
+        (
+            AppDbContext dbContext,
+            UsersRepository users,
+            RolesRepository roles,
+            TripsRepository trips
+
+        )
+        {
+            _dbContext = dbContext;
+            Users = users;
+            Roles = roles;
+            Trips = trips;
+        }
+
+        public void SaveChanges()
+        {
+            try
+            {
+                _dbContext.SaveChanges();
+            }
+            catch(Exception exception)
+            {
+                var errorMessage = "Error when saving to the database: "
+                    + $"{exception.Message}\n\n"
+                    + $"{exception.InnerException}\n\n"
+                    + $"{exception.StackTrace}\n\n";
+
+                Console.WriteLine(errorMessage);
+            }
+        }
+    }
+}
