3570da585a79da46b0aea8bccab21178d7a1ccfa
Dinescu Damian <dandreidamian@gmail.com>
2023-05-10 17:10:30 +0300

    Add User controller with login and register endpoints.

---
+++ Backend/Project/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Core.Dtos;
+using Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserService userService { get; set; }
+
+        public UsersController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public IActionResult Register([FromBody] UserRegisterDTO registerData)
+        {
+            if (registerData == null)
+            {
+                return BadRequest();
+            }
+            userService.Register(registerData);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] UserLoginDTO payload)
+        {
+            var jwtToken = userService.Validate(payload);
+            if (jwtToken == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(new {token = jwtToken});
+        }
+
+
+    }
+}
